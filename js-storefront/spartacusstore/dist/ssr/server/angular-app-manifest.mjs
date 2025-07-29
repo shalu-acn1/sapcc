468c89faf2b9fd39bf5f7cbb195c1eac89e9937b
Tripathi <shalu.tripathi@accenture.com>
2025-07-29 08:38:24 +0530

    added new dist - opf route changes

---
+++ js-storefront/spartacusstore/dist/ssr/server/angular-app-manifest.mjs
@@ -0,0 +1,253 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/',
+  locale: undefined,
+  routes: undefined,
+  entryPointToBrowserMapping: {
+  "node_modules/@spartacus/opf/fesm2022/spartacus-opf-checkout.mjs": [
+    {
+      "path": "chunk-SONTVUII.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-NGEUBE3K.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/opf/fesm2022/spartacus-opf-base.mjs": [
+    {
+      "path": "chunk-HBA3HIRO.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-4QGFQKK3.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/opf/fesm2022/spartacus-opf-payment.mjs": [
+    {
+      "path": "chunk-YVK3U2AC.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-NGEUBE3K.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-4QGFQKK3.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/opf/fesm2022/spartacus-opf-cta.mjs": [
+    {
+      "path": "chunk-PPOOKQNC.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-4QGFQKK3.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/opf/fesm2022/spartacus-opf-global-functions.mjs": [
+    {
+      "path": "chunk-EAC2753S.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/opf/fesm2022/spartacus-opf-quick-buy.mjs": [
+    {
+      "path": "chunk-F3VOGFEU.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-4QGFQKK3.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/product-configurator/fesm2022/spartacus-product-configurator-textfield.mjs": [
+    {
+      "path": "chunk-44XQY4CC.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/product-configurator/fesm2022/spartacus-product-configurator-rulebased.mjs": [
+    {
+      "path": "chunk-PTAFZPDE.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/asm/fesm2022/spartacus-asm.mjs": [
+    {
+      "path": "chunk-ZS3V6FQS.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-KPYSLQGO.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/asm/fesm2022/spartacus-asm-customer-360.mjs": [
+    {
+      "path": "chunk-ZWXXZM6T.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-KPYSLQGO.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-3ERLXEDD.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/smartedit/fesm2022/spartacus-smartedit.mjs": [
+    {
+      "path": "chunk-YS7BHPJR.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/product/fesm2022/spartacus-product-variants.mjs": [
+    {
+      "path": "chunk-DQXBEVTW.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/product/fesm2022/spartacus-product-image-zoom.mjs": [
+    {
+      "path": "chunk-JPS2V6E2.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/qualtrics/fesm2022/spartacus-qualtrics.mjs": [
+    {
+      "path": "chunk-ZX4E3ZNC.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/customer-ticketing/fesm2022/spartacus-customer-ticketing.mjs": [
+    {
+      "path": "chunk-CSRKGYOU.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/pickup-in-store/fesm2022/spartacus-pickup-in-store.mjs": [
+    {
+      "path": "chunk-GDZ24MZM.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-wish-list.mjs": [
+    {
+      "path": "chunk-OU7S63CD.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-wish-list-components-add-to-wishlist.mjs": [
+    {
+      "path": "chunk-2RYY6OHO.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-quick-order.mjs": [
+    {
+      "path": "chunk-SRMQVHAO.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-import-export.mjs": [
+    {
+      "path": "chunk-4OCTJH5Z.js",
+      "dynamicImport": false
+    }
+  ],
+  "src/app/spartacus/features/order/order-wrapper.module.ts": [
+    {
+      "path": "chunk-ROOIHGRX.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-QWRPYGZR.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/checkout/fesm2022/spartacus-checkout-base.mjs": [
+    {
+      "path": "chunk-K5PFHWKZ.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/tracking/fesm2022/spartacus-tracking-personalization.mjs": [
+    {
+      "path": "chunk-XOL4VBSJ.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/storefinder/fesm2022/spartacus-storefinder.mjs": [
+    {
+      "path": "chunk-DTQN2W7E.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-3ERLXEDD.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/cross-fetch/dist/browser-ponyfill.js": [
+    {
+      "path": "chunk-LAPENRSJ.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/user/fesm2022/spartacus-user-account.mjs": [
+    {
+      "path": "chunk-VPUBLMIV.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/user/fesm2022/spartacus-user-profile.mjs": [
+    {
+      "path": "chunk-ZLIAZZ6R.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-base.mjs": [
+    {
+      "path": "chunk-DFCWGHME.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-base-components-mini-cart.mjs": [
+    {
+      "path": "chunk-NZA2BP3T.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-base-components-add-to-cart.mjs": [
+    {
+      "path": "chunk-KU2C2IUQ.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-QWRPYGZR.js",
+      "dynamicImport": false
+    }
+  ],
+  "node_modules/@spartacus/cart/fesm2022/spartacus-cart-saved-cart.mjs": [
+    {
+      "path": "chunk-7EYMFACR.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-QWRPYGZR.js",
+      "dynamicImport": false
+    }
+  ]
+},
+  assets: {
+    'index.csr.html': {size: 27039, hash: 'a9661ccb0ca05d8c53f8f4f56a81909760bac3f1b22f666e42ac1cf76a2fd702', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1637, hash: 'e74fd7bbc60a25066e0ddf736f1e78771a9f130877337a324fd67df121e5c0de', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-Z2UGE7W2.css': {size: 925920, hash: 'g2/tJB2iiyw', text: () => import('./assets-chunks/styles-Z2UGE7W2_css.mjs').then(m => m.default)}
+  },
+};
